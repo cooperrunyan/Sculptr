e1471e915ea3dd4da2a9cdfae6d8ab90c583e0f9
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-01-21 09:43:57 -0700

    update

--- src/commands/use/index.ts
+++ src/commands/use/index.ts
@@ -14,11 +14,17 @@ export default async function use(number: string = 'latest') {
 
   if (!exists) throw new Error('Ivalid version number');
 
-  // prettier-ignore
-  if (exists) await exec(`deno install --unstable --allow-write --allow-read --allow-net --allow-run -n sculptr --allow-env -f https://deno.land/x/sculptr@${version}/src/index.ts`);
+  if (exists) {
+    await exec(
+      `deno install --unstable --allow-write --allow-read --allow-net --allow-run -n sculptr --allow-env -f https://deno.land/x/sculptr@${version}/src/index.ts`,
+    );
+    return console.log(`Successfully installed sculptr@${version}`);
+  }
 
-  if (!version)
+  if (!version) {
     await exec('deno install --unstable --allow-write --allow-read --allow-net --allow-run -n sculptr --allow-env -f https://deno.land/x/sculptr/src/index.ts');
+    return console.log(`Successfully installed sculptr@${version}`);
+  }
 
   console.log(`Successfully installed sculptr@${version}`);
 }
