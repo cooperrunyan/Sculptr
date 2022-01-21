d9a610c105184a6b895642c19ee9db8a373b4ce6
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-01-21 09:38:57 -0700

    test

--- src/commands/add/getFileJson.ts
+++ src/commands/add/getFileJson.ts
@@ -1,12 +1,14 @@
 import { root } from '../../root.ts';
 
 export default async function getFileJson(originFile: string, file: string): Promise<string | { [key: string]: any }> {
+  console.log(root);
   return root.startsWith('file://')
     ? JSON.parse(Deno.readTextFileSync(`${root.replace('file://', '')}/assets/out/files/license/${originFile}`))[file]
     : await ((await fetch(`${root}/assets/out/files/license/${originFile}`)) as any).json()[file];
 }
 
 export async function getAll(originFile: string): Promise<{ [key: string]: any }> {
+  console.log(root);
   return root.startsWith('file://')
     ? JSON.parse(Deno.readTextFileSync(`${root.replace('file://', '')}/assets/out/files/license/${originFile}`))
     : await ((await fetch(`${root}/assets/out/files/license/${originFile}`)) as any).json();
