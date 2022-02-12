71454bada3cc2f11692a2a8f90282186d4fef6e6
cooperrunyan <cooperrunyanspam@gmail.com>
2022-02-12 12:42:30 -0700

    fixed path to config.yaml

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
+import { base } from './base.ts';
 import { yaml } from './deps.ts';
 
-export default yaml.parse(Deno.readTextFileSync('../config.yaml')) as {
+export default yaml.parse(Deno.readTextFileSync(new URL(`${base}config.yaml`))) as {
   name: string;
   author: string;
   description: string;
